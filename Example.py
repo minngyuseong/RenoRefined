f363e26be028ff58fd2fc19b36a427feeefd02ea
Mingyu <minngyuseong@gmail.com>
2025-10-29 20:53:42 +0900

    Initial commit: networking labs and Reno experiments

---
+++ Example.py
@@ -0,0 +1,49 @@
+from mininet.net import Mininet
+from mininet.topo import Topo
+from mininet.node import OVSKernelSwitch
+from mininet.cli import CLI
+from mininet.node import Host
+from mininet.log import setLogLevel, info
+import socket
+import time
+
+
+class SimpleTopology(Topo):
+    def build(self):
+        h1 = self.addHost('h1', cls=Host, defaultRoute=None)
+        h2 = self.addHost('h2', cls=Host, defaultRoute=None)
+        h3 = self.addHost('h3', cls=Host, defaultRoute=None)
+        s1 = self.addSwitch( 's1', cls=OVSKernelSwitch, failMode='standalone')
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s1)
+
+
+def main():
+    topo = SimpleTopology()
+    net = Mininet( topo=topo, autoSetMacs=True, build=False, ipBase="10.0.0.0/24")
+
+#    net.build()
+    net.start()
+
+    h1 = net.getNodeByName('h1')
+    h2 = net.getNodeByName('h2')
+    h3 = net.getNodeByName('h3')
+
+    h1.setIP(intf='h1-eth0', ip='10.0.0.1/24')
+    h2.setIP(intf='h2-eth0', ip='10.0.0.2/24')
+    h3.setIP(intf='h3-eth0', ip='10.0.0.3/24')
+
+    result = h1.cmd('ping -c 1', '10.0.0.2')
+
+    # Measure the latency from the 'ping' command output
+    lines = result.split('\n')
+    print(lines[-2])
+
+    CLI(net)
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    main()
